fa6a767b2295c9360f7e2e222bcb004fbb16ab11
Yow Yiying <yowyiying@gmail.com>
2021-06-15 00:11:44 +0800

    set up bottom tab icons

--- App.js
+++ App.js
@@ -7,12 +7,38 @@ import ExploreScreen from "./Screens/ExploreScreen.js";
 import NotificationsScreen from "./Screens/NotificationsScreen.js";
 import UploadScreen from "./Screens/UploadScreen.js";
 
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            
+            //Set the icon based on which route it is (name of the tab)
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Explore') {
+              iconName = 'list';
+            } else if (route.name === 'Upload') {
+              iconName = focused ? 'user' : 'user-o';
+            } else if (route.name === 'Notifications') {
+              iconName = focused ? 'user' : 'user-o';
+            }  
+
+            // You can return any component that you like here!
+            return <FontAwesome name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'blue',
+          inactiveTintColor: 'gray',
+        }}
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Explore" component={ExploreScreen} />
         <Tab.Screen name="Upload" component={UploadScreen} />
