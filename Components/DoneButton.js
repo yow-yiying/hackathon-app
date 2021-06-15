db15495811259617006eb9192b52c06426d8d8ea
q-x-w <quahxiwen@gmail.com>
2021-06-15 15:50:52 +0800

    hello

--- Components/DoneButton.js
+++ Components/DoneButton.js
@@ -3,31 +3,16 @@ import { MaterialIcons } from '@expo/vector-icons';
 import { View, TouchableOpacity, StyleSheet, Alert, onPress, SafeAreaView,Text} from 'react-native';
 
 export default function Button() {
-    const pressHandler =(screenName) => {
-        /* if (screenName=='detailsScreen'){ //if its the explore screen click listing
-            Alert.alert('Save Changes',[
-                {text: 'Confirm', onPress:()=> console.log('this should save changes'), style:'' },
-                {text: 'Cancel', onPress:() =>console.log('this should revert to previous screen') , style:'cancel'},
-            ])
-        }
-        else{ //if its the create new listing from upload screen
-            Alert.alert('The listing cannot be deleted once created. Confirm?', [
-                {text: 'Confirm', onPress:()=> console.log('this should save changes') },
-                {text: 'Cancel', onPress:() =>console.log('this should revert to previous screen') , style:'cancel'},
-            ])
-        } */
-    }
+    
     return (
-      <SafeAreaView style={styles.container}>
-        <TouchableOpacity onPress = {pressHandler} >
+         
             <View style={styles.container}>
                 <View style={styles.confirmContainer}>
                     <Text style={styles.text}>Confirm</Text>
                     <MaterialIcons name="done" size={40} color="green" />
                 </View> 
             </View>
-        </TouchableOpacity>
-      </SafeAreaView>
+        
         
       );
 }
@@ -56,3 +41,19 @@ const styles = StyleSheet.create({
     }
 })
 
+/* <TouchableOpacity onPress = {pressHandler} > 
+ </TouchableOpacity> 
+const pressHandler =(screenName) => {} */
+
+/* if (screenName=='detailsScreen'){ //if its the explore screen click listing
+            Alert.alert('Save Changes',[
+                {text: 'Confirm', onPress:()=> console.log('this should save changes'), style:'' },
+                {text: 'Cancel', onPress:() =>console.log('this should revert to previous screen') , style:'cancel'},
+            ])
+        }
+        else{ //if its the create new listing from upload screen
+            Alert.alert('The listing cannot be deleted once created. Confirm?', [
+                {text: 'Confirm', onPress:()=> console.log('this should save changes') },
+                {text: 'Cancel', onPress:() =>console.log('this should revert to previous screen') , style:'cancel'},
+            ])
+        } */
