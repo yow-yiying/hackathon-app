46c2046aa8a126459276c565874acbb7aabfff2b
Yow Yiying <yowyiying@gmail.com>
2021-06-15 13:29:48 +0800

    added listing component

---
+++ Components/Listing.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { View, Text, Image, StyleSheet} from "react-native";
+
+export default function Listing() {
+    return (
+        <View style = {listingStyles.listingContainer}>
+            <Image style = {listingStyles.listingPhoto}
+            source = {{uri: "https://wallpaperaccess.com/full/1261215.jpg"}}/>
+            <View style = {listingStyles.listingTextContainer}>
+                <Text style = {listingStyles.listingCornerName}>Kindness Corner</Text>
+                <Text style = {listingStyles.listingOtherText}>some location</Text>
+                <Text style = {listingStyles.listingOtherText}>timing</Text>
+            </View>
+        </View>
+    )
+}
+
+const listingStyles = StyleSheet.create({
+    listingContainer: {
+        flexDirection: 'row',
+        width: '90%',
+        backgroundColor: 'pink',
+        borderRadius: 20,
+        padding: 15,
+        margin: 10,
+    }, 
+    listingPhoto: {
+        width: 100,
+        height: 100,
+        borderRadius: 10,
+    },
+    listingTextContainer: {
+        flex: 1,
+        paddingLeft: 25,
+        justifyContent: 'center',
+    },
+    listingCornerName: {
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+    listingOtherText: {
+        fontSize: 15,
+    }
+})
--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -3,10 +3,17 @@ import {Text, View, Button} from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import styles from './style.js';
 
+import ItemInput from "../Components/ItemInput.js";
+import CancelButton from "../Components/CancelButton.js";
+import Listing from "../Components/Listing.js";
+
 function HomeScreen() {
   return (
     <View style={[styles.appBackground, { flex: 1, justifyContent: 'center', alignItems: 'center' }]}>
       <Text>Home!</Text>
+      <ItemInput></ItemInput>
+      <CancelButton></CancelButton>
+      <Listing></Listing>
     </View>
   );
 }
